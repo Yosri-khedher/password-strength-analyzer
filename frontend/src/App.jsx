10f5b3cbfa28a07878d60860290a78907b6c774f
Yosri Khedher <khedheryossri@gmail.com>
2026-07-20 23:26:46 +0100

    Connect frontend to Render API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/analyze", {
+      const response = await fetch("https://password-strength-analyzer-muj4.onrender.com/analyze", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -96,12 +96,12 @@ function App() {
           className="toggle-password"
           onClick={() => setShowPassword(!showPassword)}
         >
-         {showPassword ? <FiEyeOff /> : <FiEye />}
+          {showPassword ? <FiEyeOff /> : <FiEye />}
         </button>
       </div>
 
       <button
-        onClick={handleAnalyze}
+        onClick={() => handleAnalyze()}
         disabled={loading}
       >
         {loading ? "Analyzing..." : "Analyze Password"}
